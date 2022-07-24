c7c8223bd7935d62cf1e20d6f026ac35126b63c8
haansohee <bearforlove@naver.com>
2022-07-24 20:33:29 +0900

    C#_week11

---
+++ Week11_hansohee/week11_hansohee/Dog.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace week11_hansohee
+{
+    class Animal : Object  // animal의 조상님은 object ...... 흐릿한 색은 맞긴 한데 안써도 되는거야 컴파일러가 알아서 해줌
+    {
+        private static int count = 0;
+        protected readonly int Seq;  //private는 아무리 파생된 클래스라고 해도 막ived 클래스가 여기에 접근할 권리 없음
+                                    // protected는 public과 private 사이.... 상속 없으면 protected 는 private랑 똒~같아..
+
+        public int Age { get; set; }
+
+        public void Eat()
+        {
+            Console.WriteLine("먹는다.");
+        }
+
+        public Animal(int age) : base()
+        {
+            Seq = count++;
+            Age = age;
+        }
+    }
+    class Dog : Animal  // dog는 animal를 기반으로 해서 파생된 클래스야
+    {
+        // public int Age { get; set; }  // animal한테 물려받았으니까 지워도 되겟쬬?
+        public string Color { get; set; }
+        
+        // public void Eat()
+        // {
+        //     Console.WriteLine("먹는다.");
+        // }
+
+        public void Bark()
+        {
+            Console.WriteLine("멍멍");
+        }
+
+        public Dog(int age) : base(age)  // 상속을 받은 자기 생성자 실행 전에 base 생성자 먼저 실행해야 됨
+        {
+            Console.WriteLine(Seq);
+        }
+    }    
+    
+    class Cat : Animal
+    {
+        // public int Age { get; set; }
+
+        // public void Eat()
+        // {
+        //     Console.WriteLine("먹는다.");
+        // }
+        public void Meow()
+        {
+            Console.WriteLine("야옹");
+        
+        }
+
+        public Cat(int age) : base(age)
+        {
+
+        }
+    }
+}
---
+++ Week11_hansohee/week11_hansohee/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace week11_hansohee
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Animal> animals = new List<Animal>();
+
+            animals.Add(new Dog(1) { Color = "RED" });
+            animals.Add(new Cat(2));
+
+            foreach(Animal a in animals)
+            {
+                Console.WriteLine(a.Age);
+                a.Eat();
+
+                if(a is Cat)  // a가 Cat 타입이야?
+                {
+                    ((Cat)a).Meow();  // 고양이 애들만 형변환
+                }
+
+                Dog d = a as Dog;  // as는 형변환 연산자, a변수가 알고 있는 값을 Dog 타입으로 형변환 해서 d로 넘겨줌
+                                    // 형변환 안되면 null을 return
+                if (d != null)  // null 아니면 형변환이 된 거야
+                {
+                    d.Bark();
+                    Console.WriteLine(d.Color);
+                }
+
+                // 형변환이 중요하면 as 쓰고 is...는 어떤 클래스인지가 중요하면?
+
+                
+            }
+
+            // List<Dog> dogs = new List<Dog>();
+            // List<Cat> cats = new List<Cat>();
+            // 
+            // dogs.Add(new Dog() { Age = 1, Color = "RED" });
+            // cats.Add(new Cat() { Age = 2 });
+            // 
+            // foreach(var a in dogs)
+            // {
+            //     Console.WriteLine(a.Age);
+            // }
+            // 
+            // foreach(var a in cats)
+            // {
+            //     Console.WriteLine(a.Age);
+            // }
+
+            Console.WriteLine(1.ToString());  // int
+            Console.WriteLine("1".ToString());  // string
+            Console.WriteLine(animals.ToString());  // list
+            Console.WriteLine((new Dog(0)).ToString()); ;  // Dog, dog는 내가 만들었으... 내가 만들지 않았는데 있다는 것은 상속받았다는것
+        }
+    }
+}
